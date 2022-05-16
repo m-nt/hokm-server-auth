b62abbf92273db2b1a1ff4d59bbb6aba569eedce
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2022-05-16 09:50:16 +0430

    update

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker images
 read IMAGE
 docker image rm $IMAGE
 docker build . -t server-auth
-docker run -d -p 127.0.0.1:8080:8080 --link mongodb:mongodb --name auth server-auth
+docker run -d -p 127.0.0.1:8080:8080 --restart always --link mongodb:mongodb --name auth server-auth
 docker logs -f auth
